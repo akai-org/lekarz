d968a15dbcc9f70cc37bdd8986acfbd9dc9a3d13
MichalSz99 <m.szczepaniak99@gmail.com>
2019-10-26 11:12:33 +0200

    operation model

--- interventions/models.py
+++ interventions/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Intervention(models.Model):
     name = models.CharField(max_length=255, default="zabieg", null=False)
     description = models.TextField()
     min_time = models.FloatField()
+
+
+class Operation(models.Model):
+    intervention = models.ForeignKey(Intervention, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
 
