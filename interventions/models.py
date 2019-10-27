61cbdbc4d177a50ca82a3771850432927de740ad
PatrykGrzegorek <patryk.grzegorek@o2.pl>
2019-10-27 17:59:26 +0100

    model pacjent

--- interventions/models.py
+++ interventions/models.py
@@ -14,4 +14,11 @@ class Operation(models.Model):
     date = models.DateField()
     time = models.TimeField()
 
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    disease = models.CharField(max_length=128)
+    start_date = models.DateField()
+    main_dr = models.CharField(max_length=32, default="Pole na przypisanie doktora")
+    dr_description = models.TextField()
+
 
