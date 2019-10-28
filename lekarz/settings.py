fe1e650607d4a8a7ea85e0100d3b51135b1cc4c9
PatrykGrzegorek <51320419+PatrykGrzegorek@users.noreply.github.com>
2019-10-28 13:16:21 +0100

    #16 messaging module
    * first commit dev-16
    
    * Add massage conponent

--- lekarz/settings.py
+++ lekarz/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'interventions.apps.InterventionsConfig',
+    'message.apps.MessageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
