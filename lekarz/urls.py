93ada6fa4ae52d42434b19a64512cd9c85d8caa0
Hubert Cwajda <cwajdahubert@gmail.com>
2019-10-29 18:18:21 +0100

    Patient details view and template
    Printing foreign-key doctor's card field
    
    Printing foreign-key doctor's card field #2

--- lekarz/urls.py
+++ lekarz/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import TemplateView
+from interventions.views import patient_details
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('patients/<int:patient_id>/details', patient_details),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
