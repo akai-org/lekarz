32bcc1f7f2a3b732ae95e6e16a474a059598ea08
Kapi <kacperszmitk12@gmail.com>
2019-10-29 19:29:35 +0100

    Dodoano model doktora

--- interventions/models.py
+++ interventions/models.py
@@ -14,9 +14,18 @@ class Operation(models.Model):
     date = models.DateField()
     time = models.TimeField()
 
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=64)
+    section = models.CharField(max_length=64)
+    card_number = models.IntegerField('Id lekarza')
+    
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     hospitalization_case = models.CharField(max_length=128)
     start_date = models.DateField()
-    main_dr = models.CharField(max_length=32, default="Pole na przypisanie doktora")
+    main_dr = models.ForeignKey(Doctor, on_delete=models.DO_NOTHING)
     dr_description = models.TextField()
+
+
+    
