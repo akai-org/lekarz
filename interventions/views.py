ba9b06e4e17e6be6a822a4a68dffabc70c0f36a0
Hubert Cwajda <cwajdahubert@gmail.com>
2019-10-29 18:18:21 +0100

    Patient details view and template
    Printing foreign-key doctor's card field
    
    Printing foreign-key doctor's card field #2

--- interventions/views.py
+++ interventions/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import Patient
+
+
+def patient_details(request, patient_id):
+    patient = get_object_or_404(Patient, id=patient_id)
+    return render(request, 'interventions/patient.html', {'patient': patient})
 # Create your views here.
