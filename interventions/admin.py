b570b74cab15bd82029c9b45a95c22e5cf8712f6
AleksandraSwierkowska <aleksandraswierkowska99@gmail.com>
2019-10-31 00:02:03 +0100

    Issue 12, można dodać operację do danego pacjenta, kilka zmian w models.py, żeby nazwy wyświetlały się sensownie
    W class Operation zmieniłam z usera na doctora, dodałam entery

--- interventions/admin.py
+++ interventions/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Intervention
+from .models import Intervention, Patient, Operation
 
 
 class InterventionAdmin(admin.ModelAdmin):
     fields = ['name', 'description', 'min_time']
 
 
+class OperationInLine(admin.StackedInline):
+    model = Operation
+    extra = 1
+
+
+class PatientAdmin(admin.ModelAdmin):
+    fieldsets = [('Informacje o pacjencie', {'fields':['hospitalization_case', 'start_date', 'main_dr', 'dr_description']})]
+    inlines = [OperationInLine]
+
+
+
 admin.site.register(Intervention, InterventionAdmin)
+admin.site.register(Patient, PatientAdmin)
