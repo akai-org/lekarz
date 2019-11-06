8cf61a67d8d447bd77de637ad98f96b71c31d212
Hubert Cwajda <cwajdahubert@gmail.com>
2019-11-06 05:50:23 +0100

    Dev 36 (#53)
    * Update views.py
    
    * Printing received messages

--- message/views.py
+++ message/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from message.models import Message
 
-# Create your views here.
+
+def user_inbox(request):
+    if request.user.is_anonymous:
+        return redirect('/accounts/login')
+    messages = Message.objects.filter(receiver=request.user)
+    return render(request, 'message/inbox.html', {'messages': messages})
