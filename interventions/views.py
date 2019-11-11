e16d33269cc5489ac6b25d9efd84c792f937a43a
Kacper <kacper.trebacz@bluebrick.pl>
2019-11-11 21:58:15 +0100

    blocks unwanted users

--- interventions/views.py
+++ interventions/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.exceptions import PermissionDenied
 
-from .models import Patient
+from .models import Patient,Doctor
 
 
 def patient_details(request, patient_id):
+    if not request.user.is_authenticated:
+        raise PermissionDenied()
+    user = request.user
     patient = get_object_or_404(Patient, id=patient_id)
-    return render(request, 'interventions/patient.html', {'patient': patient})
+    if user.id == patient.user.id or user.id==patient.main_dr.user.id:
+        return render(request, 'interventions/patient.html', {'patient': patient})
+    else:
+        raise PermissionDenied()
+
 # Create your views here.
