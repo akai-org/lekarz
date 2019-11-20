6fc59cd7447583eee3a252692240cf28271739ed
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2019-11-20 22:00:34 +0100

    kopia tego co jest w pacjencie

--- interventions/views.py
+++ interventions/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Patient
+from .models import Patient, Doctor
 
 
 def patient_details(request, patient_id):
     patient = get_object_or_404(Patient, id=patient_id)
     return render(request, 'interventions/patient.html', {'patient': patient})
+
+
 # Create your views here.
+
+
+def doctor_details(request, doctor_id):
+    doctor = get_object_or_404(Doctor, id=doctor_id)
+    context = {'doctor': doctor}
+    return render(request, 'interventions/doctor.html', context, content_type='application/xhtml+xml')
+
--- lekarz/urls.py
+++ lekarz/urls.py
@@ -5,6 +5,9 @@ from django.views.generic.base import TemplateView
 from interventions.views import patient_details
 from message.views import user_inbox
 
+from interventions.views import doctor_details
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
@@ -13,6 +16,8 @@ urlpatterns = [
     path('api/', include('api.urls', namespace='api')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('inbox/', user_inbox),
+
+    path('doctors/<int:patient_id>/details', doctor_details)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
