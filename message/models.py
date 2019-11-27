1754bb6fe19e4e7c7d79d5acfb7ed4fe81178ace
simek456 <48565687+simek456@users.noreply.github.com>
2019-11-27 19:42:29 +0100

    Notification (#81)
    * notification
    
    * literówka
    
    literówka

--- message/models.py
+++ message/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ManyToOne')
-    receiver = models.ManyToManyField(User, related_name='ManyToMany')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='MessagesSent')
+    receiver = models.ManyToManyField(User, related_name='MessageRecived')
     title = models.CharField(max_length=32)
     content = models.TextField()
     time = models.TimeField()
@@ -12,3 +12,14 @@ class Message(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Notification(models.Model):
+    receiver = models.ManyToManyField(User, related_name='NotificationsRecived')
+    title = models.CharField(max_length=32)
+    content = models.TimeField()
+    time = models.TimeField()
+    date = models.DateField()
+    def __str__(self):
+        return self.title
+
